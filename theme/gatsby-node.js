8d453b279814d4e0cea0057f2fff0a64fa76b009
Nathan Rothenberger <nroth10@gmail.com>
2019-09-07 12:22:37 -0700

    Remove work guide

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -57,17 +57,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
-
-/**
- * @DONE
- test query
- setup template
- test template as page
- test graphql query
- build single page query
- use in template
- build single page
- abstract into template
- build pages from node
- * @TODO
- */
