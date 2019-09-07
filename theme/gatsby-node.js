2daedcad2f5acb1c4d10b4933fd52fbf4445bce8
Nathan Rothenberger <nroth10@gmail.com>
2019-09-07 12:15:09 -0700

    Initial node setup

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -1,3 +1,72 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const songPage = path.resolve(`./src/templates/song-page.js`)
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
+
+  console.log('result', result);
+  console.log('posts', result);
+
+  posts.forEach((post, index) => {
+    console.log('post', post);
+    //   // const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    //   // const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      component: songPage,
+      context: {
+        slug: post.node.fields.slug,
+        //     // previous,
+        //     // next,
+      },
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    console.log('value', value)
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
+// TEST
 // exports.createPages = () => {
 //   console.log("ITS ALIVE!!!")
 // }
