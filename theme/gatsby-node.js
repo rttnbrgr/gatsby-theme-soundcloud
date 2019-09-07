78ff22ed80a518c99df51da1dd22da2ea1f60c7b
Nathan Rothenberger <nroth10@gmail.com>
2019-09-06 22:19:43 -0700

    Theme test

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -1,3 +1,13 @@
-exports.createPages = () => {
-  console.log("ITS ALIVE!!!")
-}
+// exports.createPages = () => {
+//   console.log("ITS ALIVE!!!")
+// }
+
+// test query
+// setup template
+// test template as page
+// test graphql query
+// build single page query
+// use in template
+// build single page
+// abstract into template
+// build pages from node
