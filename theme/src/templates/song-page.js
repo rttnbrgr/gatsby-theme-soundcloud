9f136573599741bfa67d53f77a839b06a89ec7c7
Joel <joel@joelmturner.com>
2019-09-13 21:37:57 -0700

    add embed component and url parser

--- theme/src/templates/song-page.js
+++ theme/src/templates/song-page.js
@@ -1,12 +1,19 @@
 import React from "react"
+import Embed, { soundCloudRegEx } from '../component/embed';
 
 const SongPageTemplate = props => {
   console.log('the props on the song page', props);
   const song = props.data.markdownRemark
   const siteTitle = props.data.site.siteMetadata.title
+
+  // grabs soundcloud url from html
+  const matched = song.html.match(soundCloudRegEx);
+
   return (
     <div>
       <h1>Song Page</h1>
+      <h2>Embed Test</h2>
+      <Embed url={matched[0]} size='l' />
       <h2>{siteTitle}</h2>
       <pre>
         {/* {props} */}
