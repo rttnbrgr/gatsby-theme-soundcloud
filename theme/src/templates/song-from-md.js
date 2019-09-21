79dc5e899a1b525d46ea09de66f82829cb4f643c
Nathan Rothenberger <nroth10@gmail.com>
2019-09-21 08:42:48 -0700

    Fork styles

--- theme/src/templates/song-from-md.js
+++ theme/src/templates/song-from-md.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import '../styles.css'
+import '../more-styles.css'
 import Embed from '../component/embed';
 import { soundCloudRegEx } from '../utils'
 // Little Debuggers
