9fad60c027fa7f9df86df5e4e40547984477f592
Nathan Rothenberger <nroth10@gmail.com>
2019-09-21 08:42:59 -0700

    Remove some features out of scope

--- theme/src/templates/song-from-config.js
+++ theme/src/templates/song-from-config.js
@@ -2,12 +2,9 @@ import React, { useState } from "react"
 import '../styles.css'
 import Embed from '../component/embed';
 import { soundCloudRegEx } from '../utils'
-// Little Debuggers
-const showAlbumArt = true;
-const showEmbed = true;
-const preferEmbedComponent = true;
 
 const sampleConfig = {
+  path: '/song1',
   artist: 'Lord Rolex',
   songTitle: 'Downtown. Portland',
   soundcloudUrl: 'https://soundcloud.com/req-1/downtown-portland'
@@ -16,44 +13,22 @@ const sampleConfig = {
 const SongPageTemplate = props => {
   console.log('the props on the song page', props);
   const md = props.data.markdownRemark
-  const { artist, songTitle } = md.frontmatter
-  const [isPlaying, setPlay] = useState(false);
-
+  // const { artist, songTitle } = md.frontmatter
   // grabs soundcloud url from html
-  const matched = md.html.match(soundCloudRegEx);
+  // const matched = md.html.match(soundCloudRegEx);
+
+  // pull from fake config
+  // swap this for REAL config
+  const { artist, songTitle, soundcloudUrl } = sampleConfig
+  const matched = soundcloudUrl.match(soundCloudRegEx);
 
   return (
     <div className="app--wrapper">
-      <div className="avatar--lockup">
-        <figure className="avatar--wrapper">
-          <img src="https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ" alt="Face" className="avatar" />
-        </figure>
-        <h1>{artist ? artist : 'Artist'}</h1>
-      </div>
+
+      <h1>{artist ? artist : 'Artist'}</h1>
       <div className="section-break" />
       <h2>{songTitle ? songTitle : 'Song Title'}</h2>
-
-      {showAlbumArt &&
-        /* Album artwork */
-        <>
-          <img src="https://picsum.photos/200" alt="Face" className="album-art" />
-          <button onClick={() => setPlay(!isPlaying)}>
-            <h2>{isPlaying ? 'Pause' : 'Play'}</h2>
-          </button>
-        </>
-      }
-
-      {/* New embed awesomeness */}
-      {showEmbed && preferEmbedComponent &&
-        <Embed url={matched[0]} size='l' />
-      }
-
-      {/* Soundcloud remark */}
-      {showEmbed && !preferEmbedComponent &&
-        <pre>
-          <section dangerouslySetInnerHTML={{ __html: md.html }} />
-        </pre>
-      }
+      <Embed url={matched[0]} size='l' />
 
     </div>
   )
