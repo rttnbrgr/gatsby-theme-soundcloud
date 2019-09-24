1d3d15887dd69d30ed54bb637c9e8757ff1600e6
Nathan Rothenberger <nroth10@gmail.com>
2019-09-23 19:46:29 -0700

    hide demo config

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,25 +2,30 @@ module.exports = {
   plugins: [
     {
       resolve: 'gatsby-theme-soundcloud-single',
-      options: {
-        sampleBool: true,
-        soundcloud: {
-          // basePath: 'songs',
-          tracks: [
-            {
-              // path: '/song1',
-              artist: 'Lord Rolex',
-              title: 'Downtown. Portland',
-              url: 'https://soundcloud.com/req-1/downtown-portland'
-            },
-            {
-              artist: 'Stanley Vaughn',
-              title: 'Couple Thangs',
-              url: 'https://soundcloud.com/stanleyxvaughn8/couple-thangs-produced-by-req-still-iz'
-            }
-          ]
-        }
-      }
+      // options: {
+      //   sampleBool: true,
+      //   soundcloud: {
+      //     // basePath: 'songs',
+      //     tracks: [
+      //       {
+      //         // path: '/song1',
+      //         artist: 'Lord Rolex',
+      //         title: 'Downtown. Portland',
+      //         url: 'https://soundcloud.com/req-1/downtown-portland'
+      //       },
+      //       {
+      //         artist: 'Stanley Vaughn',
+      //         title: 'Couple Thangs',
+      //         url: 'https://soundcloud.com/stanleyxvaughn8/couple-thangs-produced-by-req-still-iz'
+      //       },
+      //       {
+      //         artist: 'Tayo',
+      //         title: 'ACQ #Cypher',
+      //         url: 'https://soundcloud.com/tayox/acq-cypher-1'
+      //       },
+      //     ]
+      //   }
+      // }
     }
   ]
 }
