0090b50b652f4a2cbad24d6ac435b526d2e84382
Nathan Rothenberger <nroth10@gmail.com>
2019-09-27 17:08:40 -0700

    Update demo config

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -2,30 +2,36 @@ module.exports = {
   plugins: [
     {
       resolve: '@rttnbrgr/gatsby-theme-soundcloud',
-      // options: {
-      //   sampleBool: true,
-      //   soundcloud: {
-      //     // basePath: 'songs',
-      //     tracks: [
-      //       {
-      //         // path: '/song1',
-      //         artist: 'Lord Rolex',
-      //         title: 'Downtown. Portland',
-      //         url: 'https://soundcloud.com/req-1/downtown-portland'
-      //       },
-      //       {
-      //         artist: 'Stanley Vaughn',
-      //         title: 'Couple Thangs',
-      //         url: 'https://soundcloud.com/stanleyxvaughn8/couple-thangs-produced-by-req-still-iz'
-      //       },
-      //       {
-      //         artist: 'Tayo',
-      //         title: 'ACQ #Cypher',
-      //         url: 'https://soundcloud.com/tayox/acq-cypher-1'
-      //       },
-      //     ]
-      //   }
-      // }
+      options: {
+        soundcloud: {
+          basePath: 'acq',
+          tracks: [
+            {
+              artist: "Lord Rolex",
+              title: "Downtown. Portland",
+              url: "https://soundcloud.com/req-1/downtown-portland"
+            },
+            {
+              artist: "Norfway Nicky",
+              title: "We On (ft. Mack the Shark)",
+              url:
+                "https://soundcloud.com/tasteclub/we-on-produced-by-rolex-req"
+            },
+            {
+              artist: "Stanley x Vaughn",
+              title: "Couple Thangs",
+              url:
+                "https://soundcloud.com/stanleyxvaughn8/couple-thangs-produced-by-req-still-iz"
+            },
+            {
+              artist: "Mack the Shark",
+              title: "Alone",
+              url:
+                "https://soundcloud.com/trdwstmackrcketpowr/alone-rcket-powr-4-mixtape"
+            }
+          ]
+        }
+      }
     }
   ]
 }
