3998c3f3ebfdade7dfaaefa8c524fae76c2b72fe
Nathan Rothenberger <nroth10@gmail.com>
2019-09-30 20:42:50 -0700

    Add logic for track nav

--- theme/src/templates/soundcloudTrack.js
+++ theme/src/templates/soundcloudTrack.js
@@ -2,15 +2,38 @@ import React from "react"
 import '../styles.css'
 import Embed from '../component/embed';
 import { soundCloudRegEx } from '../utils'
+import { Link } from 'gatsby'
+// No exports from node
+// import { defaultConfig } from '../../gatsby-node';
+
+const defaultConfig = {
+  basePath: '/tracks'
+}
 
 const SongPageTemplate = props => {
-  console.log('the props on the song page', props);
-
+  // console.log('the props on the song page', props);
+  const { tracks, basePath: configBasePath } = props.data.sitePlugin.pluginOptions.soundcloud;
   const { artist, title, url } = props.pageContext.track;
   const matched = url.match(soundCloudRegEx);
 
   return (
     <div className="app--wrapper">
+      <nav className="track__nav">
+        {tracks.map((x, i) => {
+          {/* Duplicate Logic */ }
+          const basePath = configBasePath || defaultConfig.basePath;
+          const slugify = str => {
+            const slug = str
+              .toLowerCase()
+              .replace(/[^a-z0-9]+/g, "-")
+              .replace(/(^-|-$)+/g, "")
+            return `/${basePath}/${slug}`.replace(/\/\/+/g, "/")
+          }
+          return (<Link to={slugify(x.title)} className="track__link">{x.title}</Link>)
+        }
+        )}
+
+      </nav>
 
       <h1>{artist ? artist : 'Artist'}</h1>
       <div className="section-break" />
